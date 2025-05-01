45c1082d1f3f4d724687f463148beabed195414a
a <a>
2025-04-30 20:44:50 -0500

    Multi-pop fifo using XPM macros for now

--- rtl/comms/sync_fifo_multiport.vhd
+++ rtl/comms/sync_fifo_multiport.vhd
@@ -25,7 +25,7 @@ entity sync_fifo_multiport is
 		WRITE_PORTS : positive := 1;
 
 		--Depth in number of WIDTH words
-		DEPTH : positive := 256);
+		DEPTH : positive := 32);
 	port (
 		--System
 		clk : std_ulogic;
@@ -46,136 +46,113 @@ entity sync_fifo_multiport is
 end entity;
 
 architecture rtl of sync_fifo_multiport is
-	constant max_ports : natural := 4;
-	constant depth_per_rank : natural := DEPTH / max_ports;
+	constant rank : natural := 4;
+	constant depth_per_rank : natural := DEPTH / rank;
 
 	subtype ptr_t is natural range 0 to DEPTH - 1;
-	subtype rank_ptr_t is natural range 0 to DEPTH / max_ports - 1;
-	subtype data_t is std_ulogic_vector(WIDTH - 1 downto 0);
-	subtype length_t is unsigned(bits_required(READ_PORTS) - 1 downto 0);
-	
-	type mem_rank_t is array (0 to DEPTH / max_ports - 1) of data_t;
-	type rank_ptr_array_t is array (0 to max_ports - 1) of rank_ptr_t;
-	type mem_t Is array (0 to max_ports - 1) of mem_rank_t;
 
-	signal mem : mem_t;
-	signal read_rank_ptr : rank_ptr_array_t;
-	signal write_rank_ptr : rank_ptr_array_t;
+	signal read_ptr, write_ptr : ptr_t := 0;
+	signal next_read_ptr, next_write_ptr : ptr_t;
 
-	signal write_ptr_sig : ptr_t := 0;
-	signal read_ptr_sig : ptr_t := 0;
+	type fifo_ctrl_t is array (0 to rank - 1) of std_ulogic;
 
-	impure function distribute(ptr : ptr_t) return rank_ptr_array_t is
-		variable result : rank_ptr_array_t;
-
-		constant bits : natural := bits_required(DEPTH - 1);
-		variable temp : unsigned(bits - 1 downto 0);
-
-		constant rank_bits : natural := bits_required(max_ports - 1);
-		variable top_bits : unsigned(bits - 1 downto rank_bits);
-		variable bottom_bits : unsigned(rank_bits - 1 downto 0);
-	begin
-		temp := to_unsigned(ptr, bits);
-		top_bits := temp(top_bits'range);
-		bottom_bits := temp(bottom_bits'range);
-
-		for i in 0 to max_ports - 1 loop
-			if i >= to_integer(bottom_bits) then
-				result(i) := to_integer(top_bits);
-			else
-				result(i) := to_integer(top_bits + 1);
-			end if;
-		end loop;
-
-		return result;
-	end function;
+	signal wstrb, rstrb : fifo_ctrl_t;
+	signal read_data_r : std_logic_vector(read_data'range);
 begin
-	read_rank_ptr <= distribute(write_ptr_sig);
-	write_rank_ptr <= distribute(read_ptr_sig);
+	read_data <= std_ulogic_vector(read_data_r);
 
-	gen_ranks: for i in 0 to max_ports - 1 generate
-		fifo: xpm.xpm_fifo_sync
+	gen_ranks: for i in 0 to rank - 1 generate
+		fifo: xpm_fifo_sync
 		generic map (
 			WRITE_DATA_WIDTH => WIDTH,
 			READ_DATA_WIDTH => WIDTH,
-			WR_DATA_COUNT_WIDTH => 8,
-			RD_DATA_COUNT_WIDTH => 8)
+			WR_DATA_COUNT_WIDTH => bits_required(DEPTH),
+			RD_DATA_COUNT_WIDTH => bits_required(DEPTH),
+			FIFO_WRITE_DEPTH => DEPTH,
+			FIFO_MEMORY_TYPE => "distributed")
 		port map (
-			din => write_data,
-			dout => read_data(i * 8 + 7 downto i * 8),
-
 			wr_clk => clk,
-			wr_en => ,
-			rd_en => ,
+			rst => rst,
+
+			din => std_logic_vector(write_data), --Temp
+			dout => read_data_r(i * 8 + 7 downto i * 8), --Temp, Scrambled if unaligned
 			
-			);
+			wr_en => wstrb(i),
+			rd_en => rstrb(i),
+
+			sleep => '0',
+			injectsbiterr => '0',
+			injectdbiterr => '0');
 	end generate;
 
-	process (clk)
-		variable write_ptr : ptr_t;
-		variable read_ptr : ptr_t;
-
+	process (read_ptr, write_ptr, read_size, write_size)
 		variable var_write_size : natural range 0 to WRITE_PORTS;
 		variable var_read_size : natural range 0 to READ_PORTS;
+		variable read, write : boolean;
+		variable count : natural range 0 to rank;
+	begin
+		var_write_size := to_integer(write_size);
+		var_read_size := to_integer(read_size);
 
-		variable write : boolean;
-		variable read : boolean;
+		next_write_ptr <= write_ptr;
+		next_read_ptr <= read_ptr;
 
-		variable var_rank : natural range 0 to max_ports - 1;
-		variable rank_ptr : rank_ptr_array_t;
+		write := var_write_size > 0 
+			and not (
+				(write_ptr - read_ptr) mod DEPTH < 0 and 
+				(write_ptr + var_write_size - read_ptr) mod DEPTH >= 0);
+
+		read := var_read_size > 0 
+			and not (
+				(read_ptr - write_ptr) mod DEPTH <= 0 and 
+				(read_ptr + var_read_size - write_ptr) mod DEPTH > 0);
+
+		--Calculate which FIFOs to write to
+		for i in 0 to rank - 1 loop
+			wstrb(i) <= '0';
+		end loop;
+
+		if write then
+			count := 0;
+
+			for i in 0 to rank - 1 loop
+				if count < var_write_size then
+					wstrb((write_ptr + count) mod rank) <= '1';
+					count := count + 1;
+				end if;
+			end loop;
+
+			next_write_ptr <= write_ptr + count;
+		end if;
+
+		--Calculate which FIFOs to read from
+		for i in 0 to rank - 1 loop
+			rstrb(i) <= '0';
+		end loop;
+
+		if read then
+			count := 0;
+
+			for i in 0 to rank - 1 loop
+				if count < var_read_size then
+					rstrb((read_ptr + count) mod rank) <= '1';
+					count := count + 1;
+				end if;
+			end loop;
+
+			next_read_ptr <= read_ptr + count;
+		end if;
+	end process;
+
+	process (clk)
 	begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				write_ptr_sig <= 0;
-				read_ptr_sig <= 0;
+				read_ptr <= 0;
+				write_ptr <= 0;
 			else
-				var_write_size := to_integer(write_size);
-				var_read_size := to_integer(read_size);
-
-				read_ptr := read_ptr_sig;
-				write_ptr := write_ptr_sig;
-
-				write := var_write_size > 0 
-					and not (
-						(write_ptr - read_ptr) mod DEPTH < 0 and 
-						(write_ptr + var_write_size - read_ptr) mod DEPTH >= 0);
-
-				read := var_read_size > 0 
-					and not (
-						(read_ptr - write_ptr) mod DEPTH <= 0 and 
-						(read_ptr + var_read_size - write_ptr) mod DEPTH > 0);
-
-				if write then
-					for i in 0 to WRITE_PORTS - 1 loop
-						if i < var_write_size then
-							var_rank := write_ptr mod max_ports;
-
-							mem(var_rank)(read_rank_ptr(var_rank)) <= 
-								write_data(8 * i + 7 downto 8 * i);
-
-							write_ptr := write_ptr + 1;
-						end if;
-					end loop;
-				end if;
-
-				if read then
-					for i in 0 to READ_PORTS - 1 loop
-						if i < var_read_size then
-							
-							var_rank := read_ptr mod max_ports;
-
-							read_data(i * 8 + 7 downto i * 8) <= 
-								mem(var_rank)(write_rank_ptr(var_rank));
-
-							read_ptr := read_ptr + 1;
-						end if;
-					end loop;
-
-					read_valid <= '1';
-				end if;
-
-				read_ptr_sig <= read_ptr;
-				write_ptr_sig <= write_ptr;
+				read_ptr <= next_read_ptr;
+				write_ptr <= next_write_ptr;
 			end if;
 		end if;
 	end process;
